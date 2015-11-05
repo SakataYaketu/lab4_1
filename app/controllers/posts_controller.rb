0b6817daac25c324fd33cf0ae27947e5d720c465
SakataYaketu <wangxueqing@outlook.com>
2015-11-05 09:31:54 -0400

    My blog destroy action created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		#@post = Post.new(params[:post])
-		@post = Post.new(params.require(:post).permit(:title, :content))
+		@post = Post.new(params[:post])
+		#@post = Post.new(params.require(:post).permit(:title, :content))
 		if @post.save
 			redirect_to posts_path, :notice => "Successfully created!"
 		else
@@ -22,11 +22,31 @@ class PostsController < ApplicationController
 	end
 
 	def edit
+		@post = Post.find(params[:id]) 
 	end
 
 	def update
+		@post = Post.find(params[:id])  
+   
+	  	respond_to do |format|  
+		    if @post.update_attributes(params[:post])  
+		        format.html  { redirect_to(@post,  
+		                    :notice => 'Post was successfully updated.') }  
+		        format.json  { render :json => {}, :status => :ok }  
+		    else  
+		        format.html  { render :action => "edit" }  
+		        format.json  { render :json => @post.errors, :status => :unprocessable_entity }  
+		    end  
+        end  
 	end
 
 	def destroy
+		@post = Post.find(params[:id])  
+	 	@post.destroy  
+	   
+	  	respond_to do |format|  
+	    format.html { redirect_to posts_url }  
+	    format.json { head :ok }  
+	  	end  
 	end
 end
